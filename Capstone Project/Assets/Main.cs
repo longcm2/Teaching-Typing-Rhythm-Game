021c06e72c0dd1c22d3d37fb86fe8962948c76fa
cocoacacao <longcm2@appstate.edu>
2022-09-20 14:44:58 -0400

    Moving stuff from local to online

---
+++ Capstone Project/Assets/Main.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    AudioSource enigma;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enigma = GetComponent<AudioSource>();
+        enigma.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Stopwatch stopWatch = new Stopwatch();
+
+        
+    }
+}
---
+++ svp2lt.py
@@ -0,0 +1,19 @@
+import sys
+import re
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", dest = "input", default = None, help = "Path to input")
+args = parser.parse_args()
+
+file_path = Path(args.input)
+rawinput = [line.rstrip('\n') for line in open(file_path)]
+
+for word in rawinput:
+    re.sub("(.)*\{\"onset", "\{\"onset", word)
+    re.sub("\, \"detune(.)*", "", word)
+    print(word)
+
+exit()
