086b426136a02c2a18015aab903fba2c6bf7bb50
cocoacacao <longcm2@appstate.edu>
2022-10-11 16:55:33 -0400

    Bug fixes

--- Capstone Project/Assets/Main.cs
+++ Capstone Project/Assets/Main.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Text.RegularExpressions;
 using TMPro;
 
@@ -16,6 +17,8 @@ public class Main : MonoBehaviour
     public GameObject panelToAttachButtonsTo;
     public Queue<GameObject> Notes = new Queue<GameObject>();
     public Queue<GameObject> Lyrics = new Queue<GameObject>();
+    public bool active;
+    public int health = 2;
 
     //  Start is called before the first frame update
     void Start()
@@ -31,78 +34,108 @@ public class Main : MonoBehaviour
     void Update()
     {
         // If space is pressed, hide the first note and make the next one active.
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (active && Input.GetKeyDown(KeyCode.Space)) {
             Notes.Dequeue().SetActive(false);
             Lyrics.Dequeue().SetActive(false);
+            if (Notes.Count != 0) {
+                Notes.Peek().SetActive(true);
+                Lyrics.Peek().SetActive(true);
+            }
+            else {
+                active = false;
+            }
+        }
+
+        if (!active && Notes.Count != 0) {
             Notes.Peek().SetActive(true);
             Lyrics.Peek().SetActive(true);
+            active = true;
+        }
+
+        if (health == 0) {
+            // SceneManager.LoadScene("GameOver");
         }
     }
 
     ///  This function is called in Start(). It loads every note and handles their deletion.
     void loadNotes()
     {
-        double prevoffset = 15.5; // The initial offset -- hard coded for now, will be song-specific in the future.
+        double prevoffset = 0; // The initial offset -- hard coded for now, will be song-specific in the future.
+        double scaleFactor = 0.47; // The scale factor -- kinda like a tempo.
 
         // Reads and parses the given .lt file.
         using (StreamReader sr = File.OpenText("Assets/enigma.lt"))
         {
             string line;
+
+            // Used to determine when a lyric/note should be hidden.
+            int noteCount = 0;
+
             while ((line = sr.ReadLine()) != null)
             {
-                // Calculates the offset from the start.
-                string offset = Regex.Replace(line, "\\{\\\"onset\\\"\\: ", "");
-                offset = Regex.Replace(offset, "\\, \\\"duration(.)*", "");
-                double diff = (double.Parse(offset) / 1000000000) - prevoffset;
-                
-                // Calculates the duration of the note; the grace period is calculated by adding before and after this number.
-                string lengthString = Regex.Replace(line, "(.)*duration\\\"\\: ", "");
-                double length = double.Parse(Regex.Replace(offset, "\\, \\\"lyrics", "")) / 1000000000;
-
-                // Parses the lyric from the string.
-                string lyric = Regex.Replace(line, "(.)*lyrics\\\"\\: ", "");
-                lyric = Regex.Replace(lyric, "\\\"\\, \\\"phonemes(.)*", "");
-
-                // Parses the pitch from the string.
-                // TODO
-
-                // Used to determine when a lyric/note should be hidden.
-                int i = 0;
-
-                // This is a subroutine, somewhat like a lambda function, that handles the note creation and queueing.
-                IEnumerator CreateNote(string lyric_, int i)
-                {
-                    yield return new WaitForSeconds((int) diff); //  Tells this coroutine to hang on for a bit
-
-                    // Instantiate note and lyric
-                    GameObject note = Instantiate(Resources.Load("note", typeof(GameObject))) as GameObject;
-                    GameObject lyric = Instantiate(Resources.Load("lyric", typeof(GameObject))) as GameObject;
-
-                    lyric.GetComponent<TextMeshProUGUI>().SetText(lyric_);
-
-                    //temp.text = lyric_; //  Attempts to change the text in the lyric object
+                if (!line.Contains("+")) {
+                    // Calculates the offset from the start.
+                    string offset = Regex.Replace(line, "\\{\\\"onset\\\"\\: ", "");
+                    offset = Regex.Replace(offset, "\\, \\\"duration(.)*", "");
+                    double diff = (double.Parse(offset) / 1000000000) - prevoffset;
+                    diff = diff * scaleFactor;
                     
-                    // Adds the gameobjectsto their respective queues
-                    Notes.Enqueue(note);
-                    Lyrics.Enqueue(lyric);
+                    // Calculates the duration of the note; the grace period is calculated by adding before and after this number.
+                    string lengthString = Regex.Replace(line, "(.)*duration\\\"\\: ", "");
+                    double length = double.Parse(Regex.Replace(offset, "\\, \\\"lyrics", "")) / 1000000000;
+                    length = length * scaleFactor;
 
-                    // Hides the note so that the Delete function can awaken it later.
-                    note.SetActive(false);
-                    lyric.SetActive(false);
-                    // If the first note, hide this.
-                    if (i == 0) {
-                        note.SetActive(true);
-                        lyric.SetActive(true);
+                    // Parses the lyric from the string.
+                    string lyric = Regex.Replace(line, "(.)*lyrics\\\"\\: \\\"", "");
+                    lyric = Regex.Replace(lyric, "\\\"\\, \\\"phonemes(.)*", "");
+
+                    // Parses the pitch from the string.
+                    // TODO
+
+                    // This is a subroutine, somewhat like a lambda function, that handles the note creation and queueing.
+                    IEnumerator CreateNote(string lyric_, int noteCount)
+                    {
+                        yield return new WaitForSeconds((int) diff); //  Tells this coroutine to hang on for a bit
+
+                        // Instantiate note and lyric
+                        GameObject note = Instantiate(Resources.Load("note", typeof(GameObject))) as GameObject;
+                        GameObject lyric = Instantiate(Resources.Load("lyric", typeof(GameObject))) as GameObject;
+
+                        lyric.GetComponent<TextMeshProUGUI>().SetText(lyric_);
+
+                        //temp.text = lyric_; //  Attempts to change the text in the lyric object
+                        
+                        // Adds the gameobjectsto their respective queues
+                        Notes.Enqueue(note);
+                        Lyrics.Enqueue(lyric);
+
+                        // Hides the note so that the Delete function can awaken it later.
+                        note.SetActive(false);
+                        lyric.SetActive(false);
+                        // If the first note, hide this. Set the active boolean to true.
+                        if (noteCount == 0) {
+                            note.SetActive(true);
+                            lyric.SetActive(true);
+                            active = true;
+                        }
+
+                        // Assigns the objects to a canvas so they can be displayed
+                        note.transform.SetParent(panelToAttachButtonsTo.transform);
+                        lyric.transform.SetParent(panelToAttachButtonsTo.transform);
+
+                        yield return new WaitForSeconds(3); //  Tells this coroutine to hang on for a bit
+
+
+                        // Sets the health.
+                        if (note.activeSelf) {
+                            health--;
+                        }
                     }
 
-                    // Assigns the objects to a canvas so they can be displayed
-                    note.transform.SetParent(panelToAttachButtonsTo.transform);
-                    lyric.transform.SetParent(panelToAttachButtonsTo.transform);
+                    StartCoroutine(CreateNote(lyric, noteCount)); //  Starts the subroutine to display and control the note
+
+                    noteCount++; // Increment i so that only the first note is unhidden ever.
                 }
-
-                StartCoroutine(CreateNote(lyric, i)); //  Starts the subroutine to display and control the note
-
-                i++; // Increment i so that only the first note is unhidden ever.
             }
         } 
     }
