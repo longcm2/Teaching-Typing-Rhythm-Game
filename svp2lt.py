2ee6890234d630a117a78f3a41c6e164aec349d4
cocoacacao <longcm2@appstate.edu>
2022-11-08 15:27:50 -0500

    automatic scale calculation

--- svp2lt.py
+++ svp2lt.py
@@ -8,14 +8,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", dest = "input", default = None, help = "Path to input")
 parser.add_argument("-t", "--title", dest = "title", default = None, help = "Title of the song")
 parser.add_argument("-a", "--artist", dest = "artist", default = None, help = "Artist of the song")
-parser.add_argument("-s", "--scale", dest = "scale", default = None, help = "Scales the note timing -- 0.5 in most cases")
+parser.add_argument("-s", "--tempo", dest = "tempo", default = None, help = "The tempo of the song in BPM")
 parser.add_argument("-o", "--offset", dest = "offset", default = None, help = "How many seconds the notes should be offset to match the audio -- different per song, usually I reccomend 0.1 seconds before perfect")
 args = parser.parse_args()
 
 file_path = Path(args.input)
 title = Path(args.title)
 artist = Path(args.artist)
-scale = Path(args.scale)
+tempo = Path(args.tempo)
 offset = Path(args.offset)
 
 contents = open(file_path).read()
@@ -23,7 +23,7 @@ re.sub("onset", "\nonset", contents)
 
 print(contents)
 
-#print(title,"\n",artist,"\n",scale,"\n",offset,"\n")
+#print(title,"\n",artist,"\n",tempo,"\n",offset,"\n")
 
 #for word in rawinput:
 #    temp = word
